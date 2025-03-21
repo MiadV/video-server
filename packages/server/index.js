782dc73e3de7ed5bdfc7b12377688ad7b509a7ed
Miad Vosoughi <miadsoft@gmail.com>
2025-03-21 11:37:48 +0800

    update cors

--- packages/server/index.js
+++ packages/server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
